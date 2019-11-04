964855cf162ab402cf1a4a98344bd10918fd944c
KhoaTD <khoatrandinh94@gmail.com>
2019-11-04 15:00:17 +0100

    fixed door spawn in wall

--- my_script.js
+++ my_script.js
@@ -15,12 +15,26 @@ var height = 30;
 var queue = [];
 var door = [];
 
+function make_array(w, h, val) {
+    var arr = [];
+    for(let i = 0; i < h; i++) {
+        arr[i] = [];
+        for(let j = 0; j < w; j++) {
+            arr[i][j] = val;
+        }
+    }
+    return arr;
+}
+
+var maze = make_array(width, height, 0);
+
 function clear_grid(){
     queue = [];
     door = [];
     console.log("clearing", queue);
     for (let i = 0; i < height; i++){
         for (let j = 0; j < width; j++){
+            maze[i][j] = 0;
             document.getElementById(get_cell_name(i,j)).className = "unvisited";
         }
     }
@@ -65,12 +79,13 @@ function add_border(width, height) {
     }
 }
 
-function get_cell_name(x, y) {
-    return `${x}_${y}`; 
+function get_cell_name(y, x) {
+    return `${y}_${x}`; 
 }
 
-function wallify(x,y) {
-    queue.push(get_cell_name(x,y));
+function wallify(y,x) {
+    maze[y][x] = 1;
+    queue.push(get_cell_name(y,x));
     // document.getElementById(get_cell_name(x, y)).className = "wall" ;   
 }
 
@@ -79,8 +94,8 @@ function get_random_int(min, max) {
     return x;
 }
 
-function get_type(x, y){
-    return document.getElementById(get_cell_name(x, y)).className;
+function get_type(y, x){
+    return document.getElementById(get_cell_name(y, x)).className;
 }
 
 function div_maze(top_left_x, top_left_y, bottom_right_x, bottom_right_y, door_x, door_y) {
@@ -105,21 +120,28 @@ function div_maze(top_left_x, top_left_y, bottom_right_x, bottom_right_y, door_x
         for (let i = top_left_x; i < bottom_right_x+1; i++) {
             wallify(wall_y, i);
         }
-        if ((get_type(wall_y, top_left_x-1) == "unvisited")
-            && (get_type(wall_y-1, top_left_x-1) == "wall")
-            && (get_type(wall_y+1, top_left_x-1) == "wall")) {
+        if ((maze[wall_y][top_left_x-1] == 0) 
+            && (maze[wall_y-1][top_left_x-1] == 1)
+            && (maze[wall_y+1][top_left_x-1] == 1)){
+            console.log("hor NO 1");
             door_x = top_left_x;
+            door.push(get_cell_name(wall_y, door_x));
+            maze[wall_y][door_x] = 0;
         }
-        if ((get_type(wall_y, bottom_right_x+1) == 'unvisited') 
-            && (get_type(wall_y-1, bottom_right_x+1) == "wall")
-            && (get_type(wall_y+1, bottom_right_x+1) == "wall")) {
+        if ((maze[wall_y][bottom_right_x+1] == 0) 
+            && (maze[wall_y-1][bottom_right_x+1] == 1)
+            && (maze[wall_y+1][bottom_right_x+1] == 1)) {
+            console.log("hor NO 2");
             door_x = bottom_right_x;
+            door.push(get_cell_name(wall_y, door_x));
+            maze[wall_y][door_x] = 0;
         }
         if (door_x == -1) {
             door_x = get_random_int(top_left_x, bottom_right_x);
+            door.push(get_cell_name(wall_y, door_x));
+            maze[wall_y][door_x] = 0;
         }
-        document.getElementById(get_cell_name(wall_y, door_x)).className = "unvisited";
-        door.push(get_cell_name(wall_y, door_x));
+        // document.getElementById(get_cell_name(wall_y, door_x)).className = "unvisited";
         div_maze(top_left_x, top_left_y, bottom_right_x, wall_y-1, door_x, wall_y);
         div_maze(top_left_x, wall_y+1, bottom_right_x, bottom_right_y, door_x, wall_y);
     } else {
@@ -129,21 +151,28 @@ function div_maze(top_left_x, top_left_y, bottom_right_x, bottom_right_y, door_x
         }
         //add_door()
         let door_y = -1;
-        if ((get_type(top_left_y, wall_x) == 'unvisited') 
-            && (get_type(top_left_y-1, wall_x-1) == "wall")
-            && (get_type(top_left_y-1, wall_x+1) == "wall")) {
+        if ((maze[top_left_y-1][wall_x] == 0) 
+            && (maze[top_left_y-1][wall_x-1] == 1)
+            && (maze[top_left_y-1][wall_x+1] == 1)){
+            console.log("ver NO 1");
             door_y = top_left_y;
+            maze[door_y][wall_x] = 0;
+            door.push(get_cell_name(door_y, wall_x));
         }
-        if ((get_type(bottom_right_y+1, wall_x) == 'unvisited') 
-            && (get_type(bottom_right_y+1, wall_x-1) == "wall")
-            && (get_type(bottom_right_y+1, wall_x+1) == "wall")) {
+        if ((maze[bottom_right_y+1][wall_x] == 0) 
+            && (maze[bottom_right_y+1][wall_x-1] == 1)
+            && (maze[bottom_right_y+1][wall_x+1] == 1)) {
+            console.log("ver NO 2");
             door_y = bottom_right_y;
+            maze[door_y][wall_x] = 0;
+            door.push(get_cell_name(door_y, wall_x));
         }
         if (door_y == -1){
             door_y = get_random_int(top_left_y, bottom_right_y);
+            maze[door_y][wall_x] = 0;
+            door.push(get_cell_name(door_y, wall_x));
         }
-        document.getElementById(get_cell_name(door_y, wall_x)).className = "unvisited";
-        door.push(get_cell_name(door_y, wall_x));
+        // document.getElementById(get_cell_name(door_y, wall_x)).className = "unvisited";
         div_maze(top_left_x, top_left_y, wall_x-1, bottom_right_y, wall_x, door_y);
         div_maze(wall_x+1, top_left_y, bottom_right_x, bottom_right_y, wall_x, door_y);
     }
@@ -155,7 +184,7 @@ create_grid(width,height);
 //
 function mazeGenerationAnimations() {
   // let nodes = board.wallsToAnimate.slice(0);
-    let speed = 1;
+    let speed = 10;
     // console.log(queue.length);
     // console.log(queue[queue.length-1]);
     function timeout(index) {
